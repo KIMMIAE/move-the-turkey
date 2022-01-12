927e7b816b0198145d9b421798719acf10e8c913
MIAE <32053142+KIMMIAE@users.noreply.github.com>
2022-01-12 23:54:51 +0900

    step3

--- main.ts
+++ main.ts
@@ -4,12 +4,18 @@ namespace SpriteKind {
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.vy = -300
 })
+info.onCountdownEnd(function () {
+    game.over(false)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`cage`, function (sprite, location) {
     info.changeScoreBy(1)
     tiles.setTileAt(location, assets.tile`transparency16`)
     turkey = sprites.create(assets.image`turkey`, SpriteKind.Rescued)
     tiles.placeOnTile(turkey, location)
     turkey.follow(mySprite)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.swamp.swampTile16, function (sprite, location) {
+    game.over(true)
 })
 let turkey: Sprite = null
 let mySprite: Sprite = null
@@ -20,3 +26,4 @@ controller.moveSprite(mySprite)
 mySprite.ay = 500
 scene.cameraFollowSprite(mySprite)
 tiles.placeOnRandomTile(mySprite, assets.tile`start`)
+info.startCountdown(120)
